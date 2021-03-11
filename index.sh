29632ad5a214a89d980e8c0b20b13a5025d0d305
Joe Wallace <52563656+jwall3@users.noreply.github.com>
2021-03-11 17:00:40 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,7 +3,10 @@ gmx make_ndx -n index2.ndx -o index2.ndx <<EOF
 q
 EOF
 
-gmx trjconv -f inxtc -s tpr -o outxtc -n index2.ndx -dt 1000 <<EOF
+gmx editconf -f nvt.gro -o nvt_out.gro -n index2.ndx <<EOF
 24
 EOF
-                         
+
+gmx trjconv -f md_xx.xtc -s md.tpr -o mdout.xtc -n index2.ndx -dt 100 <<EOF
+24
+EOF
